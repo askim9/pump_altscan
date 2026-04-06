443016d409e10f8f72936ab5c513703014a32c7e
askim9 <askimbirdfarm@gmail.com>
2026-04-06 11:14:55 +0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -84,7 +84,7 @@ log = logging.getLogger(__name__)
 # ══════════════════════════════════════════════════════════════════════════════
 CONFIG: Dict = {
     # === API KEYS ===
-    "coinalyze_api_key": os.getenv("COINALYZE_API_KEY"),   # wajib
+    "coinalyze_api_key": os.getenv("COINALYZE_API_KEY","ab447e9a-3a26-4253-a68e-1cd0603d22d2"),   # wajib
     "bot_token":         os.getenv("BOT_TOKEN"),
     "chat_id":           os.getenv("CHAT_ID"),
 
